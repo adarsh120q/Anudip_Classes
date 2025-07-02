cfe26e368cca001e3c506d175ea840c0bb1a667f
adrsh120q <adarshsingh120q@gmail.com>
2025-07-02 14:47:32 +0530

    updated

--- Practice_Ques(1)/identify_char.py
+++ Practice_Ques(1)/identify_char.py
@@ -1,12 +1,15 @@
 a = input("Enter the character: ")
 
-if (ord(a)>=20 and ord(a)<48) or (ord(a)>57 and ord(a)<65) or (ord(a)>90 and ord(a)<97) or (ord(a)>122 and ord(a)<127):
-    print("It is a SYMBOL!")
-elif (ord(a)>47 and ord(a)<58):
-    print("It is a NUMBER!")
-elif (ord(a)>64 and ord(a)<91):
-    print("It is an UPPERCASE ALPHABET!")
-elif (ord(a)>60 and ord(a)<123):
-    print("It is a LOWERCASE ALPHABET!")
+if len(a) != 1:
+    print("Input single character only!")
 else:
-    print("Invalid input!")
+    if (ord(a)>=20 and ord(a)<48) or (ord(a)>57 and ord(a)<65) or (ord(a)>90 and ord(a)<97) or (ord(a)>122 and ord(a)<127):
+        print("It is a SYMBOL!")
+    elif (ord(a)>47 and ord(a)<58):
+        print("It is a NUMBER!")
+    elif (ord(a)>64 and ord(a)<91):
+        print("It is an UPPERCASE ALPHABET!")
+    elif (ord(a)>60 and ord(a)<123):
+        print("It is a LOWERCASE ALPHABET!")
+    else:
+        print("Invalid input!")
