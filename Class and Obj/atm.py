d4f80f285acb0913f48cc16de1278077934df253
adrsh120q <adarshsingh120q@gmail.com>
2025-07-16 23:13:19 +0530

    updated

--- Class and Obj/atm.py
+++ Class and Obj/atm.py
@@ -5,25 +5,27 @@ class As_Bank:
     def withdraw(self,amt):
         if self.balance>= amt:
             self.balance-=amt
-            return f"\nTransaction Successful!\nYour remaining balance is: {self.balance}\n"
+            return f"\nTransaction Successful!\nYour remaining balance is: ₹{self.balance}\n"
         else:
-            return "\nInsufficient balance!"
+            return f"\nInsufficient balance!\nYour current balance is: ₹{self.balance}\n"
 
     def deposit(self,amt):
         self.balance+=amt
-        return f"\nTransaction Successful!\nYour current balance is: {self.balance}\n"
+        return f"\nTransaction Successful!\nYour current balance is: ₹{self.balance}\n"
 
     def left_bal(self): 
-            return f"\nYour current balance is: {self.balance}\n"
+            return f"\nYour current balance is: ₹{self.balance}\n"
 
 print("Welcome to AS Bank!\n")
 trans = As_Bank()
 while True:
+
     print("Enter 1 to Withdraw          ","Enter 2 to Deposit")
     print("Enter 3 to View Balance      ","Enter 0 to exit the Transaction")
     choice = int(input("Enter choice number here (0-3): "))
+
     if choice == 1:
-        amt = int(input("Enter the amount you want to withdraw:"))
+        amt = int(input("Enter the amount you want to withdraw: "))
         print(trans.withdraw(amt))
         choice = int (input("Enter '1' to Continue or enter '0' to Exit the transaction: "))
         if choice == 0:
@@ -32,8 +34,9 @@ while True:
             continue
         else:
             print("\nInvalid Input!\n")
+
     elif choice == 2:
-        amt = int(input("Enter the amount you want to deposit:"))
+        amt = int(input("Enter the amount you want to deposit: "))
         print(trans.deposit(amt))
         choice = int (input("Enter '1' to Continue or enter '0' to Exit the transaction: "))
         if choice == 0:
@@ -42,6 +45,7 @@ while True:
             continue
         else:
             print("\nInvalid Input!\n")
+
     elif choice == 3:
         print(trans.left_bal())
         choice = int (input("Enter '1' to Continue or enter '0' to Exit the transaction: "))
@@ -51,6 +55,7 @@ while True:
             continue
         else:
             print("\nInvalid Input!\n")
+
     elif choice == 0:
         break
     else:
